50e82d166f878466e00a9af634f98744534702da
Joshua Ibbotson <josh@Joshuas-MacBook-Pro-2.local>
2022-05-07 09:51:55 +0100

    sumAll problem completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,36 +1,37 @@
-const sumAll = function(beginning, end) {
+const sumAll = function(beginning, end){
+  
+    if (typeof(beginning) !== "number" || beginning < 0 ||
+       typeof(end) !== "number" || end < 0){
+      return "ERROR";
+    }
     
-    if (Number.isInteger(beginning) && beginning > 0 &&
-    Number.isInteger(end) && end > 0) {
-
-    let start;
-    let finish;
-
-    if (beginning > end) {
-        start = end;
-        finish = beginning;
-    }
-    if (beginning < end){
-        start = beginning;
-        finish = end;
-    }
-
-        let total = start;
-        let sumArray = [start];
-
-        for (let i=start; i <= finish; i++){
-            total++
-            if (i < finish){
-            sumArray.push(total);
-        }
     else {
-        return "ERROR";
-    };
-    }
-    return sumArray.reduce((previousValue, currentValue)=> previousValue + currentValue);
-    }
-};
-sumAll(1, 4)
+      let start;
+      let finish;
+  
+      if (beginning > end) {
+          start = end;
+          finish = beginning;
+      }
+      else {
+          start = beginning;
+          finish = end;
+      }
+  
+          let total = start;
+          let sumArray = [start];
+  
+          for (let i=start; i <= finish; i++){
+              total++
+              if (i < finish){
+              sumArray.push(total);
+          }
+      }
+  return sumArray.reduce((previousValue, currentValue)=> previousValue + currentValue);
+  }
+    
+  }
+  sumAll ("", 10)
 
 // Do not edit below this line
 module.exports = sumAll;
