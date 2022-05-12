1aae00ee77f099dfef27c4dd05a908499d2e3e9a
Joshua Ibbotson <josh@Joshuas-MacBook-Pro-2.local>
2022-05-12 06:45:23 +0100

    tempconversion exercise complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const ftoc = function() {
+const ftoc = function(temp) {
 
+const answer = (temp - 32) * 0.5556;
+const deciimalAnswer = Number(answer.toFixed(1))
+return deciimalAnswer;
 };
 
-const ctof = function() {
+ftoc(100)
 
+const ctof = function(temp) {
+
+const answer = (temp * 1.8) + 32;
+const deciimalAnswer = Number(answer.toFixed(1))
+return deciimalAnswer;
 };
 
+
+ctof (34)
+// should return `37.8` and not `37.77777777777778`.
 // Do not edit below this line
 module.exports = {
   ftoc,
