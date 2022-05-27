e2d41f162dcd306df2bdf0d9bf882fb6d6889b42
Joshua Ibbotson <josh@Joshuas-MacBook-Pro-2.local>
2022-05-27 20:34:15 +0100

    exercise 11 completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
+  
+const getTheTitles = function(arr) {
+  let result =[];
+  for (i=0; i < arr.length; i++){
+    result.push(arr[i].title)
+  }
+  return result;
 };
+getTheTitles(books)
 
 // Do not edit below this line
 module.exports = getTheTitles;
