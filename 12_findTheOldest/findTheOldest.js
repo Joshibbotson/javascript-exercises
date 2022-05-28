eb14a0a473cabfbe63db4c880bfeb289a060e940
Joshua Ibbotson <josh@Joshuas-MacBook-Pro-2.local>
2022-05-28 17:59:44 +0100

    problem 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,41 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
-};
+
+const findTheOldest = function(arr) {
+    const date = new Date().getFullYear()
+    let ageArray =[]
+    
+      for (i=0; i <arr.length; i++) {
+      if (typeof(arr[i].yearOfDeath) !== 'undefined') {
+        ageArray.push(arr[i].yearOfDeath - arr[i].yearOfBirth)
+      }
+      else {
+        ageArray.push(date - arr[i].yearOfBirth)
+      }  
+    }
+    const max = Math.max(...ageArray);
+    const index = ageArray.indexOf(max)
+    const oldest = arr[index]
+    
+    return oldest;
+  };
+  
+  findTheOldest(people)
 
 // Do not edit below this line
 module.exports = findTheOldest;
